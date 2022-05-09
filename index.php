6fbeb49e580981de8554466c83d402ce7019d67b
Hugo <hugo.chabert@laplateforme.io>
2022-05-09 15:03:05 +0200

    Autocomplete project start

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Recherche</title>
+
+    <!-- link css-->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <!-- link js-->
+    <script src="script.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <main>
+        <div class="search-container">
+            <input type="text" name="fruit" id="fruit" placeholder="Rechercher une ville">
+            <div class="suggestions">
+                <ul></ul>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
